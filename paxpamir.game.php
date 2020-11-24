de94be992c20cab8e08cc09d8999556a78ec0c66
Abel Allison <abel@joinhonor.com>
2020-11-24 17:49:44 -0600

    Fix odd whitespace.

--- paxpamir.game.php
+++ paxpamir.game.php
@@ -3,11 +3,11 @@
   *------
   * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
   * paxpamir implementation : © Jeff DiCorpo <jdicorpo@gmail.com>
-  * 
+  *
   * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
   * See http://en.boardgamearena.com/#!doc/Studio for more information.
   * -----
-  * 
+  *
   * paxpamir.game.php
   *
   * This is the main file for your game logic.
@@ -32,8 +32,8 @@ class paxpamir extends Table
         //  the corresponding ID in gameoptions.inc.php.
         // Note: afterwards, you can get/set the global variables with getGameStateValue/setGameStateInitialValue/setGameStateValue
         parent::__construct();
-        
-        self::initGameStateLabels( array( 
+
+        self::initGameStateLabels( array(
             "remaining_actions" => 10,
 
             //    "my_first_global_variable" => 10,
@@ -42,7 +42,7 @@ class paxpamir extends Table
             //    "my_first_game_variant" => 100,
             //    "my_second_game_variant" => 101,
             //      ...
-        ) );        
+        ) );
 
         $this->tokens = new Tokens();
         // $this->tokens->initGlobalIndex('GINDEX', 0);
@@ -51,28 +51,28 @@ class paxpamir extends Table
         // $this->cards->init("cards");
 
 	}
-	
+
     protected function getGameName( )
     {
 		// Used for translations and stuff. Please do not modify.
         return "paxpamir";
-    }	
+    }
 
     /*
         setupNewGame:
-        
+
         This method is called only once, when a new game is launched.
         In this method, you must setup the game according to the game rules, so that
         the game is ready to be played.
     */
     protected function setupNewGame( $players, $options = array() )
-    {    
+    {
         // Set the colors of the players with HTML color code
         // The default below is red/green/blue/orange/brown
         // The number of colors defined here must correspond to the maximum number of players allowed for the gams
         $gameinfos = self::getGameinfos();
         $default_colors = $gameinfos['player_colors'];
- 
+
         // Create players
         // Note: if you added some extra field on "player" table in the database (dbmodel.sql), you can initialize it there.
         $sql = "INSERT INTO player (player_id, player_color, player_canal, player_name, player_avatar) VALUES ";
@@ -86,7 +86,7 @@ class paxpamir extends Table
         self::DbQuery( $sql );
         self::reattributeColorsBasedOnPreferences( $players, $gameinfos['player_colors'] );
         self::reloadPlayersBasicInfos();
-        
+
         /************ Start the game initialization *****/
 
         $this->tokens->createTokensPack("card_{INDEX}", "deck", 116);
@@ -94,7 +94,7 @@ class paxpamir extends Table
 
         // Init global values with their initial values
         self::setGameStateInitialValue( 'remaining_actions', 2 );
-        
+
         // Init game statistics
         // (note: statistics used in this file must be defined in your stats.inc.php file)
         //self::initStat( 'table', 'table_teststat1', 0 );    // Init a table statistics
@@ -112,10 +112,10 @@ class paxpamir extends Table
     }
 
     /*
-        getAllDatas: 
-        
+        getAllDatas:
+
         Gather all informations about current game situation (visible by the current player).
-        
+
         The method is called each time the game interface is displayed to a player, ie:
         _ when the game starts
         _ when a player refreshes the game page (F5)
@@ -124,16 +124,16 @@ class paxpamir extends Table
     protected function getAllDatas()
     {
         $result = array();
-    
+
         $current_player_id = self::getCurrentPlayerId();    // !! We must only return informations visible by this player !!
-    
+
         // Get information about players
         // Note: you can retrieve some extra field you added for "player" table in "dbmodel.sql" if you need it.
         $sql = "SELECT player_id id, player_score score FROM player ";
         $result['players'] = self::getCollectionFromDb( $sql );
 
         // TODO: Gather all information about current game situation (visible by player $current_player_id).
-  
+
         $players = $this->loadPlayersBasicInfos();
         foreach ( $players as $player_id => $player_info ) {
             $result['court'][$player_id] = $this->tokens->getTokensInLocation('court_'.$player_id);
@@ -161,12 +161,12 @@ class paxpamir extends Table
 
     /*
         getGameProgression:
-        
+
         Compute and return the current game progression.
         The number returned must be an integer beween 0 (=the game just started) and
         100 (= the game is finished or almost finished).
-    
-        This method is called each time we are in a game state with the "updateGameProgression" property set to true 
+
+        This method is called each time we are in a game state with the "updateGameProgression" property set to true
         (see states.inc.php)
     */
     function getGameProgression()
@@ -179,22 +179,22 @@ class paxpamir extends Table
 
 //////////////////////////////////////////////////////////////////////////////
 //////////// Utility functions
-////////////    
+////////////
 
     /*
         In this space, you can put any utility methods useful for your game logic
     */
 
-    function startsWith ($string, $startString) 
-    { 
-        $len = strlen($startString); 
-        return (substr($string, 0, $len) === $startString); 
-    } 
+    function startsWith ($string, $startString)
+    {
+        $len = strlen($startString);
+        return (substr($string, 0, $len) === $startString);
+    }
 
 
 //////////////////////////////////////////////////////////////////////////////
 //////////// Player actions
-//////////// 
+////////////
 
     /*
         Each time a player is doing some game action, one of the methods below is called.
@@ -283,26 +283,26 @@ class paxpamir extends Table
                 'player_id' => $player_id,
                 'player_name' => self::getActivePlayerName(),
             ) );
-        } 
+        }
 
         $this->gamestate->nextState( 'clean_up' );
-        
+
     }
 
     /*
-    
+
     Example:
 
     function playCard( $card_id )
     {
         // Check that this is the player's turn and that it is a "possible action" at this game state (see states.inc.php)
-        self::checkAction( 'playCard' ); 
-        
+        self::checkAction( 'playCard' );
+
         $player_id = self::getActivePlayerId();
-        
-        // Add your game logic to play a card there 
+
+        // Add your game logic to play a card there
         ...
-        
+
         // Notify all players about the card played
         self::notifyAllPlayers( "cardPlayed", clienttranslate( '${player_name} plays ${card_name}' ), array(
             'player_id' => $player_id,
@@ -310,12 +310,12 @@ class paxpamir extends Table
             'card_name' => $card_name,
             'card_id' => $card_id
         ) );
-          
+
     }
-    
+
     */
 
-    
+
 //////////////////////////////////////////////////////////////////////////////
 //////////// Game state arguments
 ////////////
@@ -359,20 +359,20 @@ class paxpamir extends Table
                 $this->tokens->moveToken($card['key'], $to_locaction);
                 $empty_top[] = $i;
                 $card_moves[] = array(
-                    'card_id' => $card['key'], 
-                    'from' => $from_location, 
+                    'card_id' => $card['key'],
+                    'from' => $from_location,
                     'to' => $to_locaction
                 );
-                
+
                 self::notifyAllPlayers( "refreshMarket", '', array(
                     'card_moves' => $card_moves,
                     'new_cards' => $new_cards,
                 ) );
-        
+
                 $this->gamestate->nextState( 'clean_up' );
                 return;
             }
-            
+
             $from_location = 'market_1_'.$i;
             $card = $this->tokens->getTokenOnLocation($from_location);
             if ($card == null) {
@@ -381,9 +381,9 @@ class paxpamir extends Table
                 $to_locaction = 'market_1_'.array_shift($empty_bottom);
                 $this->tokens->moveToken($card['key'], $to_locaction );
                 $empty_bottom[] = $i;
-                $card_moves[] = array( 
-                    'card_id' => $card['key'], 
-                    'from' => $from_location, 
+                $card_moves[] = array(
+                    'card_id' => $card['key'],
+                    'from' => $from_location,
                     'to' => $to_locaction
                 );
 
@@ -391,7 +391,7 @@ class paxpamir extends Table
                     'card_moves' => $card_moves,
                     'new_cards' => $new_cards,
                 ) );
-        
+
                 $this->gamestate->nextState( 'clean_up' );
                 return;
             }
@@ -401,7 +401,7 @@ class paxpamir extends Table
         foreach ($empty_top as $i) {
             $card = $this->tokens->pickTokensForLocation(1, 'deck', 'market_0_'.$i)[0];
             $new_cards[] = array(
-                'card_id' => $card['key'], 
+                'card_id' => $card['key'],
                 'from' => 'deck',
                 'to' => 'market_0_'.$i
             );
@@ -410,8 +410,8 @@ class paxpamir extends Table
         foreach ($empty_bottom as $i) {
             $card = $this->tokens->pickTokensForLocation(1, 'deck', 'market_1_'.$i)[0];
             $new_cards[] = array(
-                'card_id' => $card['key'], 
-                'from' => 'deck', 
+                'card_id' => $card['key'],
+                'from' => 'deck',
                 'to' => 'market_1_'.$i
             );
         }
@@ -436,18 +436,18 @@ class paxpamir extends Table
         $this->gamestate->nextState( 'next_turn' );
 
     }
-    
+
     /*
-    
+
     Example for game state "MyGameState":
 
     function stMyGameState()
     {
         // Do some stuff ...
-        
+
         // (very often) go to another gamestate
         $this->gamestate->nextState( 'some_gamestate_transition' );
-    }    
+    }
     */
 
 //////////////////////////////////////////////////////////////////////////////
@@ -456,21 +456,21 @@ class paxpamir extends Table
 
     /*
         zombieTurn:
-        
+
         This method is called each time it is the turn of a player who has quit the game (= "zombie" player).
         You can do whatever you want in order to make sure the turn of this player ends appropriately
         (ex: pass).
-        
+
         Important: your zombie code will be called when the player leaves the game. This action is triggered
         from the main site and propagated to the gameserver from a server, not from a browser.
         As a consequence, there is no current player associated to this action. In your zombieTurn function,
-        you must _never_ use getCurrentPlayerId() or getCurrentPlayerName(), otherwise it will fail with a "Not logged" error message. 
+        you must _never_ use getCurrentPlayerId() or getCurrentPlayerName(), otherwise it will fail with a "Not logged" error message.
     */
 
     function zombieTurn( $state, $active_player )
     {
     	$statename = $state['name'];
-    	
+
         if ($state['type'] === "activeplayer") {
             switch ($statename) {
                 default:
@@ -484,34 +484,34 @@ class paxpamir extends Table
         if ($state['type'] === "multipleactiveplayer") {
             // Make sure player is in a non blocking status for role turn
             $this->gamestate->setPlayerNonMultiactive( $active_player, '' );
-            
+
             return;
         }
 
         throw new feException( "Zombie mode not supported at this game state: ".$statename );
     }
-    
+
 ///////////////////////////////////////////////////////////////////////////////////:
 ////////// DB upgrade
 //////////
 
     /*
         upgradeTableDb:
-        
+
         You don't have to care about this until your game has been published on BGA.
         Once your game is on BGA, this method is called everytime the system detects a game running with your old
         Database scheme.
         In this case, if you change your Database scheme, you just have to apply the needed changes in order to
         update the game database and allow the game to continue to run with your new version.
-    
+
     */
-    
+
     function upgradeTableDb( $from_version )
     {
         // $from_version is the current version of this game database, in numerical form.
         // For example, if the game was running with a release of your game named "140430-1345",
         // $from_version is equal to 1404301345
-        
+
         // Example:
 //        if( $from_version <= 1404301345 )
 //        {
@@ -532,5 +532,5 @@ class paxpamir extends Table
 //
 
 
-    }    
+    }
 }
